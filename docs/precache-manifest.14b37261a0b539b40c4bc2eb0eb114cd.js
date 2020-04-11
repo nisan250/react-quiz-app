ddc5b524ad914f0a8c6d8368ee4cb8b5334554c3
Nisan Sabag <nisans@google.com>
2020-04-11 18:27:23 +0300

    adding build

---
+++ docs/precache-manifest.14b37261a0b539b40c4bc2eb0eb114cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b58fec2f8362ab4dc33c934d9d2c94",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "9ed940a565f48ec803e2",
+    "url": "/react-quiz-app/static/css/main.0ce4f474.chunk.css"
+  },
+  {
+    "revision": "3a707e8b1fe2d1c9686f",
+    "url": "/react-quiz-app/static/js/2.8a7bd409.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz-app/static/js/2.8a7bd409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed940a565f48ec803e2",
+    "url": "/react-quiz-app/static/js/main.6b848ad8.chunk.js"
+  },
+  {
+    "revision": "fe0c24ce51172b7ae8b4",
+    "url": "/react-quiz-app/static/js/runtime-main.513f9891.js"
+  }
+]);
